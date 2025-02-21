201a90f4a63afac1824002d0f3febd73ed1227b8
Recon1991 <kalanicook@yahoo.com>
2025-02-20 14:37:53 -1000

    added advance tooltips

--- src/main/java/net/reconhalcyon/practicemod/block/custom/MorphiteMagicBlock.java
+++ src/main/java/net/reconhalcyon/practicemod/block/custom/MorphiteMagicBlock.java
@@ -1,20 +1,25 @@
 package net.reconhalcyon.practicemod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.reconhalcyon.practicemod.block.ModBlocks;
 import net.reconhalcyon.practicemod.item.ModItems;
+
+import java.util.List;
 
 public class MorphiteMagicBlock extends Block {
     public MorphiteMagicBlock(Properties properties) {
@@ -40,5 +45,11 @@ public class MorphiteMagicBlock extends Block {
         }
 
         super.stepOn(level, pos, state, entity);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("tooltip.recontutmod.morphite_magic_block.tooltip"));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
--- src/main/java/net/reconhalcyon/practicemod/item/ModItems.java
+++ src/main/java/net/reconhalcyon/practicemod/item/ModItems.java
@@ -1,13 +1,18 @@
 package net.reconhalcyon.practicemod.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.reconhalcyon.practicemod.PracticeMod;
 import net.reconhalcyon.practicemod.item.custom.ChiselItem;
 import net.reconhalcyon.practicemod.item.custom.FuelItem;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(PracticeMod.MOD_ID);
@@ -21,7 +26,13 @@ public class ModItems {
             () -> new ChiselItem(new Item.Properties().durability(1024)));
 
     public static final DeferredItem<Item> POKE_BOWL = ITEMS.register("poke_bowl",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.POKE_BOWL)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.POKE_BOWL)){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.recontutmod.poke_bowl.tooltip"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     public static final DeferredItem<Item> HOT_STONE = ITEMS.register("hot_stone",
             () -> new FuelItem(new Item.Properties(), 800));
